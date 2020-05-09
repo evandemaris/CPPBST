f4fd1eea801d12a3f91533d00d8afd88190feb68
Evan Demaris <evandemaris@gmail.com>
2020-05-09 11:44:40 -0700

    Initial repo committed

---
+++ BST.cpp
@@ -0,0 +1,3 @@
+#include "BST.h"
+
+//A Binary Search Tree is a data structure that stores items in memory with keys in sorted order, allowing lookup and other options to use the principle of binary search.
---
+++ BST_Node.cpp
@@ -0,0 +1,1 @@
+#include "BST_Node.h"
